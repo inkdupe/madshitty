ca95c77c8176462d41c383b9d9eee1196068c178
sentinal <inkdupe1@gmail.com>
2025-08-22 16:37:48 +0200

    Create hookmetamethod.lua

---
+++ hookmetamethod.lua
@@ -0,0 +1,17 @@
+return function(Signal)
+    local OldNamecall
+    OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(Self, ...)
+        local Method = getnamecallmethod()
+
+        if Method == "FireServer" then
+            local Results = {Signal(Self, ...)}
+            if #Results == 0 then
+                return
+            else
+                return OldNamecall(table.unpack(Results))
+            end
+        end
+
+        return OldNamecall(Self, ...)
+    end))
+end
